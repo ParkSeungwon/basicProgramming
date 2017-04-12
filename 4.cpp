dcfcffb224c733907c2261f43c726a598f2cbc72
zezeon <zezeon@msn.com>
2017-04-12 14:42:22 +0900

    0412-

--- 4.cpp
+++ 4.cpp
@@ -1,16 +1,21 @@
 //2016110056 박승원
 #include<iostream>
 using namespace std;
-
-void germs(int& bacteria, int& virus) {
-	bacteria -= virus;
-	bacteria *= 2;
-	virus *=2;
+int k=0;
+bool expect(int m, int n) {
+	k++;
+	if(!m && !n) return false;
+	if(m>0 && n>0)  return !(expect(m-1, n) && expect(m,n-1) && expect(m-1,n-1));
+	else if(!m) return !expect(m, n-1);
+	else if(!n) return !expect(m-1,n);
 }
 
 int main()
 {
-	int bacteria=100000, virus=1, minute;
-	for(minute=0; bacteria>0; minute++) germs(bacteria, virus);
-	cout << minute << endl;
+	expect(9,9);
+//	for(int i=0; i<10; i++) {
+//		for(int j=0; j<10; j++) cout << expect(j, i) << ' ';
+//		cout << endl;
+//	}
+	cout << k << endl;
 }
